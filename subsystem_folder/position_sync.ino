947380019af1c883636f4d27ba72601a8610cb0f
DobbyJT <162532671+DobbyJT@users.noreply.github.com>
2024-05-14 23:30:10 +0900

    Update position_sync.ino
    interrupt resister add

--- subsystem_folder/position_sync.ino
+++ subsystem_folder/position_sync.ino
@@ -1,14 +1,13 @@
-//pin
 #define Motor_L_pin2 3
-#define EN_L 9
-#define Encoder_L_PulseA 18
+#define EN_L 9 
+#define Encoder_L_PulseA 18 // INT3
 #define Motor_R_pin2 5
 #define EN_R 10
-#define Encoder_R_PulseA 20
+#define Encoder_R_PulseA 20 // INT1
 
 #define PWM_L 240
 
-//Encoder Count
+// encodercount
 volatile long EncoderCount_L = 0;
 volatile long EncoderCount_R = 0;
 
@@ -28,12 +27,12 @@ void setup() {
   pinMode(Encoder_L_PulseA, INPUT);
   pinMode(Encoder_R_PulseA, INPUT);
 
-  //left motor drive
   digitalWrite(Motor_L_pin2, HIGH);
   analogWrite(EN_L, PWM_L);
+  init_INT();
 
-  attachInterrupt(digitalPinToInterrupt(Encoder_L_PulseA), EncoderPositionRead_L, RISING);
-  attachInterrupt(digitalPinToInterrupt(Encoder_R_PulseA), EncoderPositionRead_R, RISING);
+  //global interrupt enable
+  SREG |= 0x01 << SREG_I;
 }
 
 void loop() {
@@ -42,27 +41,38 @@ void loop() {
   float kd = 6.8;
   float output = PID_pos_sync(EncoderCount_L, kp, kd, ki);
   Motor_R_Drive(output);
-  //for serial plotter
+
+  // 시리얼 플로터를 위한 데이터 출력
   Serial.print("EncoderCount_L:");
   Serial.print(EncoderCount_L);
   Serial.print(",");
   Serial.print("EncoderCount_R:");
   Serial.println(EncoderCount_R);
+
 }
 
-void EncoderPositionRead_L() {
+void init_INT(){
+  EICRA |= (1 << ISC30) | (1 << ISC31); //falling
+  EICRA |= (1 << ISC10) | (1 << ISC11); //falling
+  EIMSK |= (1 << INT3); //local interrpt for 3
+  EIMSK |= (1 << INT1); //local interrpt for 1
+}
+
+//INT3 ISR
+ISR(INT3_vect) {
   EncoderCount_L++;
 }
-void EncoderPositionRead_R() {
+
+//INT1 ISR
+ISR(INT1_vect) {
   EncoderCount_R++;
 }
 
 float PID_pos_sync(int target, float kp, float kd, float ki) {
-  long currentTime = micros(); //for package in function use
+  long currentTime = micros(); 
   float dt = ((float)(currentTime - preT)) / 1.0e6;
-  //target and encodercount_r all integal
-  int error = target - EncoderCount_R; 
-  e_integral = e_integral + error * dt;
+  int error = target - EncoderCount_R;
+  e_integral += error * dt;
   float e_derivative = (error - pre_error) / dt;
   float output = (kp * error) + (kd * e_derivative) + (ki * e_integral);
 
@@ -72,18 +82,18 @@ float PID_pos_sync(int target, float kp, float kd, float ki) {
   return output;
 }
 
-void Motor_R_Drive(float output){
-  //for absolute
-  if (output<0) output = output*(-1);
+void Motor_R_Drive(float output) {
+  if (output < 0) output = -output;
   int pwm_R = output;
 
-  if (EncoderCount_R > EncoderCount_L){
+  if (EncoderCount_R > EncoderCount_L) {
     pwm_R = 0;
-  }
-  else if (pwm_R > 255){
+  } else if (pwm_R > 255) {
     pwm_R = 255;
   }
-  digitalWrite(Motor_R_pin2, HIGH);
-  analogWrite(EN_R, pwm_R);
+  
+  digitalWrite(Motor_R_pin2, HIGH); //PORT 활용
+  analogWrite(EN_R, pwm_R); //duty cycle 활용
 }
+
 
