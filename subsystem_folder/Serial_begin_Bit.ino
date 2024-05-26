0722bc3384b3d846e149972b7294c91737c5dfbb
DobbyJT <162532671+DobbyJT@users.noreply.github.com>
2024-05-26 15:21:03 +0900

    Update Serial_begin_Bit.ino
    to function

--- subsystem_folder/Serial_begin_Bit.ino
+++ subsystem_folder/Serial_begin_Bit.ino
@@ -1,16 +1,21 @@
 #define FOSC 16000000 
-#define BAUD 9600
+#define BAUD 115200
 void setup(){
   //instead of Serial.begin(9600);
+  serial_baud();
+}
+
+void loop(){
+  Serial.println("done");
+  delay(1000);
+}
+
+
+void serial_baud(void){
   uint16_t ubrr = FOSC / 16 / BAUD - 1;
   UBRR0H = (unsigned char)(ubrr>>8);
   UBRR0L = (unsigned char)ubrr;
   UCSR0B |= (1<<RXEN0) | (1<<TXEN0);
   UCSR0C |= (1<<USBS0);
   UCSR0C |= (1<<UCSZ01) | (1<<UCSZ00);
-}
-
-void loop(){
-  Serial.println("done");
-  delay(1000);
 }
